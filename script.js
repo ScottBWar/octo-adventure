c3f6bfbda83f4a7c585a9408dee2a8c0bc8d3553
Scott Warner <scottbwarner@gmail.com>
2015-09-18 16:09:15 -0400

    zurb tabs setup

--- script.js
+++ script.js
@@ -1,37 +1,40 @@
 $(document).ready(function() {
 
-    $('#aboutTab').click(function() {
-        $('#contactTab').removeClass('is-active');
-        $('#workTab').removeClass('is-active');
-        $('#aboutTab').addClass('is-active');
 
-        $('#contact').removeClass('active');
-        $('#work').removeClass('active');
-        $('#about').addClass('active');
+    // $(document).foundationTabs();
 
-    });
+    // $('#aboutTab').click(function() {
+    //     $('#contactTab').removeClass('is-active');
+    //     $('#workTab').removeClass('is-active');
+    //     $('#aboutTab').addClass('is-active');
 
+    //     $('#contact').removeClass('active');
+    //     $('#work').removeClass('active');
+    //     $('#about').addClass('active');
 
-
-    $('#contactTab').click(function() {
-        $('#aboutTab').removeClass('is-active');
-        $('#workTab').removeClass('is-active');
-        $('#contactTab').addClass('is-active');
-
-        $('#about').removeClass('active');
-        $('#work').removeClass('active');
-        $('#contact').addClass('active');
-    });
+    // });
 
 
-    $('#workTab').click(function() {
-        $('#contactTab').removeClass('is-active');
-        $('#aboutTab').removeClass('is-active');
-        $('#workTab').addClass('is-active');
 
-        $('#contact').removeClass('active');
-        $('#about').removeClass('active');
-        $('#work').addClass('active');
-    });
+    // $('#contactTab').click(function() {
+    //     $('#aboutTab').removeClass('is-active');
+    //     $('#workTab').removeClass('is-active');
+    //     $('#contactTab').addClass('is-active');
+
+    //     $('#about').removeClass('active');
+    //     $('#work').removeClass('active');
+    //     $('#contact').addClass('active');
+    // });
+
+
+    // $('#workTab').click(function() {
+    //     $('#contactTab').removeClass('is-active');
+    //     $('#aboutTab').removeClass('is-active');
+    //     $('#workTab').addClass('is-active');
+
+    //     $('#contact').removeClass('active');
+    //     $('#about').removeClass('active');
+    //     $('#work').addClass('active');
+    // });
 
 });
